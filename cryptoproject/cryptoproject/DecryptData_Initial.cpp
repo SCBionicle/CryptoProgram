e09cd08467f112dcf7adc85b13810fc92fecdf6b
unknown <daj177@N0000A061.cs.utsarr.net>
2018-04-10 13:47:42 -0500

    Signed-off-by: unknown <daj177@N0000A061.cs.utsarr.net>

--- cryptoproject/cryptoproject/DecryptData_Initial.cpp
+++ cryptoproject/cryptoproject/DecryptData_Initial.cpp
@@ -18,70 +18,73 @@ int decryptData(char *data, int dataLength)
 		// (gptrPasswordHash or gPasswordHash), (gptrKey or gkey), gNumRounds
 
 		
-		AND ecx, 0 //clear ecx from any residual data from prior operations 
+		xor ecx, ecx //clear ecx from any residual data from prior operations 
 		mov edi, data				// Put ADDRESS of first data element into edi,
-		Start : // start of the loop  
-	
-		//xor byte ptr[edi + ecx], 'A'		// Seth - Exclusive-or byte
-			mov bl, byte ptr[edi + ecx] //move data byte to bl (part of ebx) to rotate
-			ror bl, 1
+	Start : // start of the loop  
+//xor byte ptr[edi + ecx], 'C' 
+		mov al, byte ptr[edi + ecx]
+			push ecx //store ecx for later use
+			xor dl, dl  // set bl to 0x00
+			mov cx, 8 //set up loop
+		CLOOP :
+			  rcr al, 1
+			  rcl dl, 1
+			  loop CLOOP // Do 8 times
+			  pop ecx //restore ecx 
+			  mov	byte ptr[edi + ecx], dl
+
+//xor byte ptr[edi + ecx], 'D'		//Eddie
+			  mov al, byte ptr[edi + ecx]
+			  mov bl, al
+			  mov dl, al
+			  and dl, 0xF0
+			  and bl, 0x0F
+			  rol bl, 4
+			  ror dl, 4
+			  rol bl, 1// left shift 
+			  jc CARRYSET //  if there is a one proceed to function
+			  jnc CONTINUE// else continue to next operation
+
+		  CARRYSET ://if cf has a one
+		or bl, 0x10 //set 5th bit to 1
+			xor bl, 0x01//clear lower nibble
+			jmp CONTINUE//continue with program
+		CONTINUE :
+		ror dl, 1 // shift right 
+			jc CARRYDL// if cf is set 
+			jnc LAST//else finish step 
+
+		CARRYDL :
+		or dl, 0x08// set 4th bit to 1
+			xor dl, 0x80// clear upper nibble 
+			jmp LAST
+		LAST :
+		rol dl, 4 // shift nibbles to upper half
+			ror bl, 4// shift nibbles to lower half
+			add bl, dl// put back together and added it back to the data
 			mov byte ptr[edi + ecx], bl
-			//xor byte ptr[edi + ecx], 'E'
-			/*
-			//xor byte ptr[edi + ecx], 'B' - Seth
+//xor byte ptr[edi + ecx], 'B' - Seth /
 			mov bl, byte ptr[edi + ecx] //bh = upper nibble, bl = lower nibble
 			mov bh, bl
 			shr bh, 4 //upper->lower
 			shl bl, 4//lower->upper
 			add bl, bh //add lower to upper
 			mov byte ptr[edi + ecx], bl //move back to memory
-			*/
-			//xor byte ptr[edi + ecx], 'D'		//Eddie
-			mov al, byte ptr[edi + ecx]
-			mov bl, al
-			mov dl, al
-			and dl, 0xF0
-			and bl, 0x0F
-			ror dl, 4
-			rol bl, 5// left shift 
-			jc CARRYSET //  if there is a one proceed to function
-			jnc CONTINUE// else continue to next operation
-
-			CARRYSET ://if cf has a one
-				or bl, 0x10 //set 5th bit to 1
-				xor bl, 0x01//clear lower nibble
-				jmp CONTINUE//continue with program
-			CONTINUE :
-				ror dl, 1 // shift right 
-				jc CARRYDL// if cf is set 
-				jnc LAST//else finish step 
-
-			CARRYDL:
-				or dl, 0x08// set 4th bit to 1
-				xor dl, 0x80// clear upper nibble 
-				jmp LAST
-			LAST:
-				rol dl,4 // shift nibbles to upper half
-				ror bl,4// shift nibbles to lower half
-				add bl, dl// put back together and added it back to the data
-				mov byte ptr[edi + ecx], bl
-
-	//xor byte ptr[edi + ecx], 'C'
-				mov al, byte ptr[edi + ecx]
-				push ecx //store ecx for later use
-				xor bl, bl  // set bl to 0x00
-				mov cx,8 //set up loop
-				CLOOP:
-					rcr al, 1
-					rcl bl, 1
-					loop CLOOP // Do 8 times
-				pop ecx //restore ecx 
-				mov	byte ptr[edi + ecx], bl
-		
+//xor byte ptr[edi + ecx], 'E' MAX
+			lea esi,  gDecodeTable
+			movzx edx, dl
+			mov  byte ptr[esi  +ecx], dl
+//////////////////////////////////////////////////////////////////////////////////////////////////	
+		//xor byte ptr[edi + ecx], 'A'		// Seth - Exclusive-or byte
+			mov bl, byte ptr[edi + ecx] //move data byte to bl (part of ebx) to rotate
+			ror bl, 1
+			mov byte ptr[edi + ecx], bl
+	
+/////////////////////////////////////////////////////////////////////////////////////////////////////		
 				inc ecx
 				cmp ecx, dataLength // check to see if we have reached the end of the data file 
 				jb Start // jump to start of loop if ecx is smaller than datalength 
-
+	
 	}
 	return resulti;
 } // decryptData
--- cryptoproject/cryptoproject/EncryptData_Initial.cpp
+++ cryptoproject/cryptoproject/EncryptData_Initial.cpp
@@ -18,39 +18,7 @@ int encryptData(char *data, int dataLength)
 	// Set up the stack frame and assign variables in assembly if you need to do so
 	// access the parameters BEFORE setting up your own stack frame
 	// Also, you cannot use a lot of global variables - work with registers
-	/*
-	__asm {
-		// you will need to reference some of these global variables
-		// (gptrPasswordHash or gPasswordHash), (gptrKey or gkey), gNumRounds
-
-		// simple example that xors 2nd byte of data with 14th byte in the key file
-		lea esi,gkey				// put the ADDRESS of gkey into esi
-		mov esi,gptrKey;			// put the ADDRESS of gkey into esi (since *gptrKey = gkey)
-
-		lea	esi,gPasswordHash		// put ADDRESS of gPasswordHash into esi
-		mov esi,gptrPasswordHash	// put ADDRESS of gPasswordHash into esi (since unsigned char *gptrPasswordHash = gPasswordHash)
-
-		mov al,byte ptr [esi]				// get first byte of password hash
-		mov al,byte ptr [esi+4]				// get 5th byte of password hash
-		mov ebx,2h
-		mov al,byte ptr [esi+ebx*2]			// get 5th byte of password hash
-		mov al,byte ptr [esi+ebx]			// get 3rd byte of password has
-
-		mov ax,word ptr [esi+ebx*2]			// gets 5th and 6th bytes of password hash ( gPasswordHash[4] and gPasswordHash[5] ) into ax
-		mov eax,dword ptr [esi+ebx*2]		// gets 4 bytes, as in:  unsigned int X = *( (unsigned int*) &gPasswordHash[4] );
-
-		mov al,byte ptr [gkey+ebx]			// get's 3rd byte of gkey[] data
-
-		mov al,byte ptr [gptrKey+ebx]		// THIS IS INCORRECT - will add the address of the gptrKey global variable (NOT the value that gptrKey holds)
-
-		mov al,byte ptr [esi+0xd];			// access 14th byte in gkey[]: 0, 1, 2 ... d is the 14th byte
-		mov edi,data				// Put ADDRESS of first data element into edi
-		xor byte ptr [edi+1],al		// Exclusive-or the 2nd byte of data with the 14th element of the keyfile
-									// NOTE: Keyfile[14] = 0x21, that value changes the case of a letter and flips the LSB
-									// Capital "B" = 0x42 becomes lowercase "c" since 0x42 xor 0x21 = 0x63
-									
-	}
-	*/
+	
 	__asm {// you will need to reference some of these global variables
 		// (gptrPasswordHash or gPasswordHash), (gptrKey or gkey), gNumRounds
 		
@@ -58,21 +26,24 @@ int encryptData(char *data, int dataLength)
 		xor ecx, ecx //clear ecx from any residual data from prior operations 
 		mov edi, data				// Put ADDRESS of first data element into edi,
 		Start: // start of the loop 
-		///////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
 		//xor byte ptr[edi + ecx], 'A' //Seth		// Exclusive-or byte
 			mov bl, byte ptr[edi + ecx] //move data byte to bl (part of ebx) to rotate
 			rol bl, 1
 			mov byte ptr[edi + ecx], bl
-			//xor byte ptr[edi + ecx], 'E'
-			
-			//xor byte ptr[edi + ecx], 'B' //Seth
+			//xor byte ptr[edi + ecx], 'E'MAX
+			lea  esi, gEncodeTable
+			movzx edx, dl
+			mov byte ptr[esi+ecx], dl
+			//xor byte ptr[edi + ecx], 'B' //Seth 
 			mov bl, byte ptr[edi + ecx] //bh = upper nibble, bl = lower nibble
 			mov bh, bl
 			shr bh, 4 //upper->lower
 			shl bl, 4//lower->upper
 			add bl, bh //add lower to upper
 			mov byte ptr[edi+ecx], bl //move back to memory
-	////////////////////////////////////////////////////////////////////////////////////
+	
+////////////////////////////////////////////////////////////////////////////////////
 			//xor byte ptr[edi + ecx], 'D'
 			mov al, byte ptr[edi + ecx]
 			mov bl, al
@@ -98,18 +69,18 @@ int encryptData(char *data, int dataLength)
 			add bl, dl
 			mov byte ptr[edi + ecx], bl
 
-		//xor byte ptr[edi + ecx], 'C'
+		//xor byte ptr[edi + ecx], 'C' 
 			mov al, byte ptr[edi + ecx]
 			push ecx;
-			xor bl, bl  // bl to zero	 
+			xor dl, dl  // bl to zero	 
 			mov cx,8
 			CLOOP:
 				rcr al, 1
-				rcl bl, 1
+				rcl dl, 1
 				loop CLOOP // Do 8 times
 			pop ecx //restore ecx for outer loop index	
-			mov	byte ptr[edi + ecx], bl		
-		
+			mov	byte ptr[edi + ecx], dl		
+			
 			inc ecx
 			cmp ecx, dataLength // check to see if we have reached the end of the data file 
 			jb Start // jump to start of loop if ecx is smaller than datalength
