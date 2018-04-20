bf630b8961c86131e1d13efbf89a8c59f1c9edd5
unknown <daj177@N0000A053.cs.utsarr.net>
2018-04-20 04:37:02 -0500

    milestone 3 Signed-off-by: unknown <daj177@N0000A053.cs.utsarr.net>

--- cryptoproject/cryptoproject/DecryptData_Initial.cpp
+++ cryptoproject/cryptoproject/DecryptData_Initial.cpp
@@ -11,105 +11,115 @@ int decryptData(char *data, int dataLength)
 {
 	int resulti = 0;
 	int index = 0;
+	int iNumOfRounds = gNumRounds;
 	gdebug1 = 0;					// a couple of global variables that could be used for debugging
 	gdebug2 = 0;					// also can have a breakpoint in C code
 
 	__asm {// you will need to reference some of these global variables
 		// (gptrPasswordHash or gPasswordHash), (gptrKey or gkey), gNumRounds
-
 		xor ecx, ecx //clear ecx from any residual data from prior operations 
+		mov edi, data				// Put ADDRESS of first data element into edi,
+	
+		SetIndex :
+			xor edx, edx
 			xor ebx, ebx
 			xor eax, eax
-			mov edi, data				// Put ADDRESS of first data element into edi,
-		Round :
-		lea esi, gPasswordHash
-			mov ah, [esi]
-			mov al, [esi + 1]
-			mov bh, [esi + 2]
-			mov bl, [esi + 3]
-			lea esi, gkey
-			test bx, bx
+			lea esi, gPasswordHash
+			mov edx, iNumOfRounds
+			//starting index
+			mov ah, [esi + edx * 4]
+			mov al, [esi + edx * 4 + 1]
+			mov [esp+8],eax
+			//hop count 
+			mov bh, [esi + edx * 4 + 2]
+			mov bl, [esi + edx * 4 + 3]
+			mov [esp+4], ebx
+			cmp bx, 0
 			jne Start
 			mov bx, 0xFFFF
-			mov dl, byte ptr[edi+ecx]
-			xor dl, byte ptr[esi + eax]
-			mov byte ptr[edi+ecx],dl
-	Start : // start of the loop  
-//xor byte ptr[edi + ecx], 'C' 
-		mov dl, byte ptr[edi + ecx]
-		xor dh, dh  // set bl to 0x00
-		push eax
-		xor eax, eax 
+			mov[esp + 4], ebx
+		Start : // start of the loop  
+		//xor byte ptr[edi + ecx], 'C' 
+		mov al, byte ptr[edi + ecx]
+		xor dl, dl  // set bl to 0x00
+		xor bl, bl
 		CLOOP :
-			rcr dl, 1
-			rcl dh, 1
-			inc eax
-			cmp eax, 8
+			rcr al, 1
+			rcl dl, 1
+			inc bl
+			cmp bl, 8
 			jne CLOOP // Do 8 times
-			pop eax
-			mov	byte ptr[edi + ecx], dh
+			mov	byte ptr[edi + ecx], dl
+		//xor byte ptr[edi + ecx], 'D'		//Eddie
+			mov al, byte ptr[edi + ecx] // grab byte from data
+			mov bl, al 
+			mov dl, al
+			and dl, 0xF0 //clear lower nibble
+			and bl, 0x0F//clear upper nibble
+			rol bl, 4 // shift to lower nibble
+			ror dl, 4// shift to higher nibble
+			rol bl, 1// left shift xxx0-000x
+			jc CARRYSET //  if there is a one proceed to function
+			jnc CONTINUE// else continue to next operation
 
-
-//xor byte ptr[edi + ecx], 'D'		//Eddie
-			  mov dl, byte ptr[edi + ecx]
-			  mov dh, dl
-			  and dl, 0xF0
-			  and dh, 0x0F
-			  rol dh, 4
-			  ror dl, 4
-			  rol dh, 1// left shift 
-			  jc CARRYSET //  if there is a one proceed to function
-			  jnc CONTINUE// else continue to next operation
-
-		  CARRYSET ://if cf has a one
-			or dh, 0x10 //set 5th bit to 1
-			xor dh, 0x01//clear lower nibble
+		CARRYSET ://if cf has a one
+			or bl, 0x10 //set 5th bit to 1,  1xxx-0001
+			xor bl, 0x01//clear lower nibble, 1xxx-0000
 			jmp CONTINUE//continue with program
 		CONTINUE :
-		ror dl, 1 // shift right 
+			ror dl, 1 // shift right x000-0xxx
 			jc CARRYDL// if cf is set 
 			jnc LAST//else finish step 
 
 		CARRYDL :
-		or dl, 0x08// set 4th bit to 1
-			xor dl, 0x80// clear upper nibble 
+			or dl, 0x08// set 4th bit to 1 1000-1xxx
+			xor dl, 0x80// clear upper nibble 0000-1xxx
 			jmp LAST
 		LAST :
-		rol dl, 4 // shift nibbles to upper half
-			ror dh, 4// shift nibbles to lower half
-			add dh, dl// put back together and added it back to the data
-			mov byte ptr[edi + ecx], dh
-//xor byte ptr[edi + ecx], 'B' - Seth /
-			mov dl, byte ptr[edi + ecx] //bh = upper nibble, bl = lower nibble
-			mov dh, dl
-			shr dh, 4 //upper->lower
-			shl dl, 4//lower->upper
-			add dl, dh //add lower to upper
-			mov byte ptr[edi+ecx], dl //move back to memory
-//xor byte ptr[edi + ecx], 'E' MAX
-			mov dl, byte ptr[edi+ecx] 
-			lea esi,  gDecodeTable
+			rol dl, 4 // shift nibbles back to upper half
+			ror bl, 4// shift nibbles back to lower half
+			add bl, dl// put back together and added it back to the data
+			mov byte ptr[edi + ecx], bl
+		//xor byte ptr[edi + ecx], 'B' - Seth /
+			mov bl, byte ptr[edi + ecx] //bh = upper nibble, bl = lower nibble
+			mov bh, bl
+			shr bh, 4 //upper->lower
+			shl bl, 4//lower->upper
+			add bl, bh //add lower to upper
+			mov byte ptr[edi + ecx], bl //move back to memory
+		//xor byte ptr[edi + ecx], 'E' MAX
+			mov dl, byte ptr[edi + ecx]
+			lea esi, gDecodeTable
 			movzx edx, dl
 			mov  dl, byte ptr[esi + edx]
-			mov byte ptr[edi+ecx], dl
-//////////////////////////////////////////////////////////////////////////////////////////////////	
-		//xor byte ptr[edi + ecx], 'A'		// Seth - Exclusive-or byte
-			mov dl, byte ptr[edi + ecx] //move data byte to bl (part of ebx) to rotate
-			ror dl, 1
 			mov byte ptr[edi + ecx], dl
-			jmp Check
-/////////////////////////////////////////////////////////////////////////////////////////////////////	
-		Check:
-			dec index 
-			cmp index, 0
-			jne Round
-			je Done
-		Done:
-				inc ecx
-				cmp ecx, dataLength // check to see if we have reached the end of the data file 
-				jb Start // jump to start of loop if ecx is smaller than datalength 
-	
-	}
+			//////////////////////////////////////////////////////////////////////////////////////////////////	
+		//xor byte ptr[edi + ecx], 'A'		// Seth - Exclusive-or byte
+			mov bl, byte ptr[edi + ecx] //move data byte to bl (part of ebx) to rotate
+			ror bl, 1
+			mov byte ptr[edi + ecx], bl	
+		Hop:
+			mov eax, [esp + 8]//load index 
+			mov ebx, [esp + 4]//load hop count
+		Hopping:
+			xor edx, edx
+			lea esi, gkey//load global key into esi
+			mov dl, byte ptr[edi + ecx]
+			xor dl, byte ptr[esi + eax]
+			mov byte ptr[edi + ecx], dl
+			add eax, ebx //add hop count to starting point
+			cmp eax, 65537//cmp to max 
+			jb Increment// if below 
+			sub eax, 65537 // if greater subtract by this num
+		Increment:
+			inc ecx
+			cmp ecx, dataLength // check to see if we have reached the end of the data file 
+			jb Start // jump to start of loop if ecx is smaller than datalength 
+		Rounds://check to see if there are more rounds to go 
+			dec iNumOfRounds
+			cmp iNumOfRounds, 0
+			jne SetIndex
+			}
 	return resulti;
 } // decryptData
 
--- cryptoproject/cryptoproject/EncryptData_Initial.cpp
+++ cryptoproject/cryptoproject/EncryptData_Initial.cpp
@@ -11,105 +11,124 @@ int encryptData(char *data, int dataLength)
 {
 	int resulti = 0;
 	gdebug1 = 0;					// a couple of global variables that could be used for debugging
-	gdebug2 = 0;					// also can have a breakpoint in C code
-
-	int index= gNumRounds;
+	gdebug2 = 0;					// also can have a breakpoint in C code 
+ 
+	int index = 0;
+	int iNumOfRounds = 0;
 	// You can not declare any local variables in C, but should use resulti to indicate any errors
 	// Set up the stack frame and assign variables in assembly if you need to do so
 	// access the parameters BEFORE setting up your own stack frame
 	// Also, you cannot use a lot of global variables - work with registers
-	
+
 	__asm {// you will need to reference some of these global variables
 		// (gptrPasswordHash or gPasswordHash), (gptrKey or gkey), gNumRounds
-		
+
 		// simple example that xors 2nd byte of data with 14th byte in the key file
-		xor ecx, ecx //clear ecx from any residual data from prior operations 
-		xor ebx, ebx
-		xor eax, eax
-		mov edi, data				// Put ADDRESS of first data element into edi,
-	Round:
-		lea esi, gPasswordHash
-		mov ah, [esi]
-		mov al, [esi+1]
-		mov bh, [esi+2]
-		mov bl, [esi+3]
-		lea esi, gkey
-		test bx, bx
-		jne Start
-		mov bx, 0xFFFF
-		mov dl, byte ptr[edi + ecx]
-		xor dl, byte ptr[esi + eax]
-		mov byte ptr[edi + ecx], dl
-		Start: // start of the loop 
-////////////////////////////////////////////////////////////////////////////////////////
-		//xor byte ptr[edi + ecx], 'A' //Seth		// Exclusive-or byte
-			mov dl, byte ptr[edi + ecx] //move data byte to bl (part of ebx) to rotate
-			rol dl, 1
+		mov edi, data// Put ADDRESS of first data element into edi,
+		SetIndex :
+			xor ecx, ecx //clear ecx from any residual data from prior operations 
+			xor edx, edx
+			xor ebx, ebx
+			xor eax, eax
+			lea esi, gPasswordHash
+			mov edx, iNumOfRounds
+			//starting index
+			mov ah, [esi + edx * 4]
+			mov al, [esi + edx * 4 + 1]
+			mov [esp-8],eax
+			//hop count 
+			mov bh, [esi + edx * 4 + 2]
+			mov bl, [esi + edx * 4 + 3]
+			mov [esp-4], ebx
+			cmp bx, 0
+			jne Hopping
+			mov bx, 0xFFFF
+			mov[esp - 4], ebx
+			jmp Hopping
+		Hop:
+			mov eax, [esp - 8]//load index 
+			mov ebx, [esp - 4]//load hop count
+		Hopping:
+			xor edx, edx
+			lea esi, gkey//load global key into esi
+			mov dl, byte ptr[edi + ecx]
+			xor dl, byte ptr[esi + eax]
 			mov byte ptr[edi + ecx], dl
-			//xor byte ptr[edi + ecx], 'E'MAX
-			mov dl, byte ptr[edi+ecx]
+			add eax, ebx //add hop count to starting point
+			cmp eax, 65537//cmp to max 
+			jb Start// if below 
+			sub eax, 65537 // if greater subtract by this num
+			jmp Start //encryting steps
+		
+		Rounds://check to see if there are more rounds to go 
+			inc iNumOfRounds
+			mov edx, gNumRounds
+			cmp iNumOfRounds, edx
+			je Done
+			jne SetIndex
+		
+		Start : // start of the loop 	
+			mov[esp - 8], eax//save next index to xor with into stack
+			xor eax, eax 
+		//xor byte ptr[edi + ecx], 'A' //Seth		// Exclusive-or byte
+			mov bl, byte ptr[edi + ecx] //move data byte to bl (part of ebx) to rotate
+			rol bl, 1
+			mov byte ptr[edi + ecx], bl
+		//xor byte ptr[edi + ecx], 'E'MAX
+			mov dl, byte ptr[edi + ecx]
 			lea  esi, gEncodeTable
 			movzx edx, dl
-			mov dl, byte ptr[esi+edx]
-			mov byte ptr[edi+ecx], dl
-			//xor byte ptr[edi + ecx], 'B' //Seth 
-			mov dl, byte ptr[edi + ecx] //bh = upper nibble, bl = lower nibble
-			mov dh, dl
-			shr dh, 4 //upper->lower
-			shl dl, 4//lower->upper
-			add dl, bh //add lower to upper
-			mov byte ptr[edi + ecx], dl //move back to memory
-	
-////////////////////////////////////////////////////////////////////////////////////
-			//xor byte ptr[edi + ecx], 'D'
-			mov dl, byte ptr[edi + ecx]
-			mov dh, dl
-			and dh, 0x0F//higher bits cleared
-			and dl, 0xF0 //lower bits cleared
-			rol dl, 1
+			mov dl, byte ptr[esi + edx]
+			mov byte ptr[edi + ecx], dl
+		//xor byte ptr[edi + ecx], 'B' //Seth 
+			mov bl, byte ptr[edi + ecx] //bh = upper nibble, bl = lower nibble
+			mov bh, bl
+			shr bh, 4 //upper->lower
+			shl bl, 4//lower->upper
+			add bl, bh //add lower to upper
+			mov byte ptr[edi + ecx], bl //move back to memory
+		//xor byte ptr[edi + ecx], 'D'
+			mov al, byte ptr[edi + ecx] // grab byte from data
+			mov bl, al 
+			mov dl, al
+			and bl, 0x0F//clear higher nibble 0000-xxxx
+			and dl, 0xF0//clear lower nibble  xxxx-0000
+			rol dl, 1// xxx0-000x
 			jc CARRYSET
 			jnc CONTINUE
-		CARRYSET:
-			or dl, 0x10
-			xor dl,0x01
+		CARRYSET :
+			or dl, 0x10 //set 5th bit to 1 xxx1-0001
+			xor dl, 0x01//set first bit to 0 xxx1-0000
 			jmp CONTINUE
-		CONTINUE:
-			ror dh, 1
+		CONTINUE :
+			ror bl, 1// x000-0xxx
 			jc CARRYBL
 			jnc LAST
-		CARRYBL:
-			or dl, 0x08
-			xor dl,0x80
+		CARRYBL :
+			or bl, 0x08// set 4th bit to 1 1000-1xxx
+			xor bl, 0x80//set 7th bit to 0 0000-1xxx
 			jmp LAST
-		LAST: 
-			add dh, dl
-			mov byte ptr[edi + ecx], dh
+		LAST :
+			add dl, bl// set back together xxxx-xxxx
+			mov byte ptr[edi + ecx], dl
 
 		//xor byte ptr[edi + ecx], 'C' 
-		mov dl, byte ptr[edi + ecx]
-		push eax 
-		xor eax, eax 
-		xor dh, dh  // set bl to 0x00
-			CLOOP :
-				rcr dl, 1
-				rcl dh, 1
-				inc eax
-				cmp eax, 8
-				jne CLOOP // Do 8 times
-			pop eax
-			mov	byte ptr[edi + ecx], dh
-			jmp Check
-		Check:
-			dec index 
-			cmp index, 0
-			jne Round
-			je Done
-		Done:
+			mov al, byte ptr[edi + ecx]
+			xor dl, dl  // set bl to 0x00
+			xor bl, bl
+		CLOOP :
+			rcr al, 1
+			rcl dl, 1
+			inc bl
+			cmp bl, 8
+			jne CLOOP // Do 8 times
+			mov	byte ptr[edi + ecx], dl
 			inc ecx
 			cmp ecx, dataLength // check to see if we have reached the end of the data file 
-			jb Start // jump to start of loop if ecx is smaller than datalength
+			jb HOP //go onto next hop 
+			jmp Rounds// check to see if we need to do another round of encryption
+		DONE:
+			
 	}
 	return resulti;
 } // encryptData
-//aebdc - Ortiz
-//
